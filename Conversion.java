74f1fdb649ccaca5a969f793d3983b37a6f94b9c
Susan Wairimu <59855870+Susan56789@users.noreply.github.com>
2020-08-28 15:49:20 +0300

    Add files via upload

---
+++ Conversion.java
@@ -0,0 +1,15 @@
+class Conversion {
+    public static void main(final String arg[]) {
+        int km;
+        int m;
+        m=1;
+         km= 1000*m;
+
+        if (m < km){
+            System.out.println("m is less than km");
+        }
+        
+    }
+
+    
+}
---
+++ exercise1.java
@@ -0,0 +1,14 @@
+class exercise1{
+    public static void main(String args[]){
+        double x,y;
+        float a;
+      String  s;
+        x=4.5;
+        y=45.0;
+        a= 752.5f;
+        s = "susan";
+        System.out.println("x is "+ x +" amd y is "+y);
+        System.out.println("a is "+a);
+        System.out.println("my name is " + s);
+    }
+}
